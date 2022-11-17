ce6d2bb72d2fa76196712969f42f4930e9724018
mourad-aziz <mourad.aziz@laplateforme.io>
2022-11-17 09:11:07 +0100

    test

--- jour03/Job04/index.php
+++ jour03/Job04/index.php
@@ -5,7 +5,8 @@ Créez un algorithme qui parcourt, compte et affiche le nombre de caractères pr
 dans cette chaîne.*/
 $str= "Dans l'espace, personne ne vous entend crier";
 for ($i = 0; isset($str[$i]) ; $i+=1) {
-    if (isset($str[$i] == null)) {
+    if (($str[$i] ) !== null)
+    {
           echo "$i <br>";
 };
 
