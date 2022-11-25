b34eb555dbc4c38097959a3f966cc2f6dc3e2d83
mourad-aziz <mourad.aziz@laplateforme.io>
2022-11-25 14:43:27 +0100

    add job6

---
+++ jour10/job05/index.php
@@ -0,0 +1,27 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "jour09";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT prenom, nom, naissance, sexe, email from etudiants WHERE DATE_FORMAT(FROM_DAYS(DATEDIFF(now(),naissance)), '%Y')+0 < 18 ";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    echo "<table><tr><th>Prénom</th><th>Nom</th><th>Naissance</th><th>Sexe</th><th>Email</th><tr>";
+    while($row = $result->fetch_assoc()) {
+        echo "<tr><td>".$row["prenom"]."</td><td>".$row["nom"]."</td><td>".$row["naissance"]."</td><td>".$row["sexe"]."</td><td>".$row["email"]."</td></tr>";
+    }
+    echo "</table>";
+} else {
+    echo "0 results";
+}
+$conn->close();
+?>
